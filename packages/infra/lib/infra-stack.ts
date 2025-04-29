3d9de5dd920ee9593aac21481cb4bcb69e5e67e5
mzkmnk <sma0729mex@gmail.com>
2025-04-30 08:36:55 +0900

    feat: create infra stack

--- packages/infra/lib/infra-stack.ts
+++ packages/infra/lib/infra-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    const webSiteBucket = new s3.Bucket(this, 'Bucket', {
+      websiteErrorDocument: 'index.html',
+      websiteIndexDocument: 'index.html',
+    })
+    
+    const webSiteIdentify = new cloudfront.OriginAccessIdentity(this, 'WebSiteIdentify', {comment: 'WebSiteIdentify'})
+    
+    const webSiteBucketPolicy = new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      effect: iam.Effect.ALLOW,
+      principals: [
+        webSiteIdentify.grantPrincipal
+      ],
+      resources: [`${webSiteBucket.bucketArn}/*`]
+    })
+    
+    webSiteBucket.addToResourcePolicy(webSiteBucketPolicy);
+    
+    const webSiteDistribution = new cloudfront.Distribution(this, 'WebSiteDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: cloudfrontOrigins.S3BucketOrigin.withOriginAccessControl(webSiteBucket)
+      },
+    })
+    
+    new s3Deployment.BucketDeployment(this, 'S3Deployment', {
+      sources: [s3Deployment.Source.asset('../frontend/dist/frontend/browser')],
+      destinationBucket: webSiteBucket,
+      distribution: webSiteDistribution,
+    })
   }
 }
